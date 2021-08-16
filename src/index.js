5da3350c1b32992e6fe107f197e47a6512896ea2
Miho F <85437491+funamioh@users.noreply.github.com>
2021-08-16 22:28:27 +0900

    Added API integration to forecast part

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-let now = new Date();
-let currentTime = document.querySelector("#currentTime");
-
-let hours = now.getHours();
-if (hours < 10) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
   hours = `0${hours}`;
 }
 let minutes = now.getMinutes();
@@ -19,14 +18,52 @@ let days = [
   "Friday",
   "Saturday"
 ];
-let day = days[now.getDay()];
-currentTime.innerHTML = `${day} ${hours}:${minutes}`;
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+  
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay) {
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML = 
+      forecastHTML +
+                       `
+                        <div class="col-2">
+                            <div class="weather-forecast-date">${day}</div><img class="below-weather-icon" src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"">
+                            <div class="temperature"><span class="weather-forecast-temp-min">7°</span>|<span class="weather-forecast-temp-max">15°</span></div>
+                        </div>`;
+});
+
+forecastHTML = forecastHTML + `</div>`;
+forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "730afeb398d3874cb3c0cb8d98df8b85";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
-  console.log(response);
   let temperatureElement = document.querySelector("#current-temp");
   let cityElement = document.querySelector("#title-city");
   let descriptionElement = document.querySelector("#description");
+  let precipitationElement = document.querySelector("#precipitation");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let iconElement = document.querySelector("#icon");
@@ -36,6 +73,7 @@ function displayTemperature(response) {
   temperatureElement.innerHTML = Math.round(celsiusTemp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  precipitationElement.innerHTML = response.data.weather[0]
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   iconElement.setAttribute("src" , `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
@@ -78,6 +116,7 @@ let currentLocationButton = document.querySelector("#current-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
 searchCity("Tokyo, Japan");
+
 
 function convertToFahrenheit(event) {
   event.preventDefault();
